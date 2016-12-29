ac15d1a924bcadbccd801d1b3ba537cba0c57c54
fumdiali <dialichukwufumnanya@gmail.com>
2016-12-29 18:50:54 +0100

    removed 'done' from code

--- lists_prac.py
+++ lists_prac.py
@@ -1,3 +1,4 @@
+#no error checking
 #does not validate user input
 #memory intensive,if user inputs millions of numbers
 name = input("Enter a user name:")
@@ -5,17 +6,18 @@ print("*****************")
 print(" Welcome,"+name)
 print("*****************")
 print("Give me some numbers,I will tell you their average..")
-print("Enter 'done' when all are in.")
+print("Enter '0' when all are in.")
 
 numlist = list()
 
 while True:
   user_input = input("Enter a number: ")
-  if user_input == 'done':
+  if user_input == '0':
     break
   value = float(user_input)
   numlist.append(value)
 
+#sum() and len() adds the entries and counts them for us,respectively
 avg = sum(numlist)/len(numlist)
 
 print("The average of your "+str(len(numlist))+"  numbers is "+str(avg))
